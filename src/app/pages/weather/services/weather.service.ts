8889d398c13f222974a8e094d42e98392988836c
Maria Rabanales Gonzalez <maria.rabanalesgonzalez@plexus.local>
2022-04-27 11:28:23 +0200

    Interceptor

--- src/app/pages/weather/services/weather.service.ts
+++ src/app/pages/weather/services/weather.service.ts
@@ -7,15 +7,12 @@ import { Observable } from "rxjs";
 @Injectable({ providedIn: 'root'})
 export class WeatherService {
     private readonly API_URL = environment.openWeather.url;
-    private readonly API_KEY = environment.openWeather.key;
 
     constructor(private readonly http: HttpClient){}
 
     public getWeatherByName(city: string): Observable<WeatherData>{
         const params = new HttpParams()
             .set('q', city)
-            .set('unit', 'metric')
-            .set('appid', this.API_KEY)
         return this.http.get<WeatherData>(`${this.API_URL}/weather`, { params });
     };
 
@@ -23,8 +20,6 @@ export class WeatherService {
         const params = new HttpParams()
             .set('lat', coord.lat)
             .set('lon', coord.lon)
-            .set('unit', 'metric')
-            .set('appid', this.API_KEY)
         return this.http.get<WeatherData>(`${this.API_URL}/weather`, { params });
     };
 
--- src/app/pages/weather/weather.module.ts
+++ src/app/pages/weather/weather.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { WeatherRoutingModule } from './weather-routing.module';
 import { WeatherComponent } from './weather.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { WeatherInterceptor } from './interceptors/weather.interceptor';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,9 @@ import { WeatherComponent } from './weather.component';
   ],
   exports: [
     WeatherComponent
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: WeatherInterceptor, multi: true }
   ]
 })
 export class WeatherModule { }
